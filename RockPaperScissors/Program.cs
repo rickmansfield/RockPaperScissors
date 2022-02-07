46ef143906cea10395b86ab01e381600cdfe1792
Rick Mansfield <PEACEFULRICK@msn.com>
2022-02-07 07:57:03 -0600

    Adds a do/while loop to continue the game at the end

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -15,57 +15,63 @@ namespace RockPaperScissors
             string playerChoice;
             int playerValue = -1;
 
-            int computerValue = randomNumbers.Next(3);
-            string computerChoice;
+            do
+            {
 
-            if (computerValue == Rock)
-            {
-                computerChoice = "rock";
-            }
-            else if (computerValue == Paper)
-            {
-                computerChoice = "paper";
-            }
-            else
-            {
-                computerChoice = "scissors";
-            }
 
-            Console.Clear();
-            Console.Write("Please enter rock, paper or scissors ");
-            playerChoice = Console.ReadLine().ToLower();
 
-            if (playerChoice.Equals("rock"))
-            {
-                playerValue = Rock;
-            }
-            else if (playerChoice.Equals("paper"))
-            {
-                playerValue = Paper;
-            }
-            else if (playerChoice.Equals("scissors"))
-            {
-                playerValue = Scissors;
-            }
-            else
-            {
-                Console.WriteLine($"{playerChoice} is not a valid choice");
-            }
+                int computerValue = randomNumbers.Next(3);
+                string computerChoice;
 
-            Console.WriteLine($"Computer chose {computerChoice}, player chose {playerChoice}");
+                if (computerValue == Rock)
+                {
+                    computerChoice = "rock";
+                }
+                else if (computerValue == Paper)
+                {
+                    computerChoice = "paper";
+                }
+                else
+                {
+                    computerChoice = "scissors";
+                }
 
-            if (playerValue == computerValue)
-            {
-                Console.WriteLine("It's a draw");
-            }
-            else if ((playerValue - 1 == computerValue) || (playerValue == Rock && computerValue == Scissors))
-            {
-                Console.WriteLine("Player Wins");
-            }
-            else
-            {
-                Console.WriteLine("The computer wins!");
-            }
+                Console.Clear();
+                Console.Write("Please enter rock, paper or scissors ");
+                playerChoice = Console.ReadLine().ToLower();
+
+                if (playerChoice.Equals("rock"))
+                {
+                    playerValue = Rock;
+                }
+                else if (playerChoice.Equals("paper"))
+                {
+                    playerValue = Paper;
+                }
+                else if (playerChoice.Equals("scissors"))
+                {
+                    playerValue = Scissors;
+                }
+                else
+                {
+                    Console.WriteLine($"{playerChoice} is not a valid choice");
+                }
+
+                Console.WriteLine($"Computer chose {computerChoice}, player chose {playerChoice}");
+
+                if (playerValue == computerValue)
+                {
+                    Console.WriteLine("It's a draw");
+                }
+                else if ((playerValue - 1 == computerValue) || (playerValue == Rock && computerValue == Scissors))
+                {
+                    Console.WriteLine("Player Wins");
+                }
+                else
+                {
+                    Console.WriteLine("The computer wins!");
+                }
+            } while (GetYesOrNo("Would you like to play again"));
         }
         /**
 * Returns a boolean response to a yes/no question.
